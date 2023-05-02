06338097849369052464b5bd9122f18709feee11
hengyu <1183660933@qq.com>
2023-05-03 01:36:54 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("add new sequence");
-    System.out.println("Test3");
+    System.out.println("Test5");
     System.out.println("Test4");
   }
 }
