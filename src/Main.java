f1cf6283cf1c34819cb9b07e6201eac1b9cd66ca
hengyu <1183660933@qq.com>
2023-05-03 01:35:27 +0800

    rebase master

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("add new sequence------");
+    System.out.println("add new sequence1");
+    System.out.println("add new sequence");
     System.out.println("Test5");
     System.out.println("Test4");
   }
