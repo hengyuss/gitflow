1ee1b84aa9e44eb689fc8757c393201195482a3f
hengyu <1183660933@qq.com>
2023-05-03 01:38:34 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("add new sequence");
+    System.out.println("add new sequence------");
     System.out.println("Test5");
     System.out.println("Test4");
   }
